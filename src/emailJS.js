374f0b6dd8dc2ef9dd2ca7f624eefe865832c4bd
Kevin <mediaman502@aol.com>
2025-03-22 22:08:13 -0400

    updated for big screens

--- src/emailJS.js
+++ src/emailJS.js
@@ -7,9 +7,9 @@ export const sendEmail = (e, form) => {
     loading.classList += ` modal__overlay--visible`
 
     emailjs.sendForm(
-        process.env.REACT_APP_SERVICE_ID, 
-        process.env.REACT_APP_TEMPLATE_ID, 
-        form.current, 
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
         process.env.REACT_APP_PUBLIC_KEY
     )
       .then((result) => {
